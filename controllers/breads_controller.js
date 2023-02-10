097d4b9900a94324daff33a35063f45e88c0cb8a
Andrew Olivares <Wrestler533438@gmail.com>
2023-02-10 09:53:41 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,16 +1,25 @@
 const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+const bread_router = express.Router()
+const bread_data = require('../models/bread.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-  res.send(Bread)
+// bread_router.get('/', (req, res) => {
+//   res.send(bread_data)
+// })
+
+// INDEX
+bread_router.get('/', (req, res) => {
+    res.render('index', {
+        "breads": bread_data
+    })
+  // res.send(Bread)
 })
 
+
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+bread_router.get('/:arrayIndex', (req, res) => {
+    res.send(bread_data[req.params.arrayIndex])
   })
   
 
-module.exports = breads
+module.exports = bread_router
